d45addb4e9f27d471829fd742cd8516d6db97f3c
NausAlexus <naus.lexus@mail.ru>
2025-03-06 23:30:52 +0300

    dsfds

---
+++ js/sliderHeader.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const slides = document.querySelectorAll('.slide-header');
+    let currentSlide = 0;
+
+    function showSlide(index) {
+        slides.forEach(slide => slide.classList.remove('active-header'));
+        slides[index].classList.add('active-header');
+
+        const offset = -index * 100;
+        document.querySelector('.slider-header-content').style.transform = `translateX(${offset}%)`;
+    }
+
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide(currentSlide);
+    }
+
+    setInterval(nextSlide, 5000);
+
+    showSlide(currentSlide);
+});
